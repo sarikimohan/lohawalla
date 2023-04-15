86dab234aa7426b8ca99b35aec481aa2503027fe
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-04-15 22:42:08 +0530

    configured the navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import style from "./App.module.css";
 import ScreenContainer from "./Components/common/Layout/ScreenContainer/ScreenContainer";
 import Sidebar from "./Components/common/Sidebar/Sidebar";
 import DashBoardScreenPath from "./screens/Dashboard/ScreenPath.constant";
 import CategoriesScreenPath from "./screens/Categories/ScreenPath.constant";
 import Categories from "./screens/Categories/Categories";
+import CategorySpecification from "./screens/CategorySpecification/CategorySpecification";
 
 //* screen imports
 const LazyDashboard = React.lazy(() => import("./screens/Dashboard/Dashboard"));
 const LazyCategories = React.lazy(
 	() => import("./screens/Categories/Categories")
+);
+const LazyCategorySpecification = React.lazy(
+	() => import("./screens/CategorySpecification/CategorySpecification")
+);
+
+const Suspense = (props: { children: React.ReactNode }) => (
+	<React.Suspense fallback="loading...">{props.children}</React.Suspense>
 );
 
 function App() {
@@ -32,13 +40,22 @@ function App() {
 								}
 							/>
 						</Route>
-						<Route path={CategoriesScreenPath()}>
+
+						<Route path={"/categories"}>
 							<Route
-								index
+								path=""
 								element={
 									<React.Suspense fallback="loading...">
 										<LazyCategories />
 									</React.Suspense>
+								}
+							/>
+							<Route
+								path=":id"
+								element={
+									<Suspense>
+										<LazyCategorySpecification />
+									</Suspense>
 								}
 							/>
 						</Route>
