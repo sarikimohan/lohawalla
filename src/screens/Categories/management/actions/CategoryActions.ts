ebf214d973c24b2bbafe26d7661b9b239b5cef6e
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-04-15 16:26:53 +0530

    added the implementation for the interface of Categories.Actions

--- src/screens/Categories/management/actions/CategoryActions.ts
+++ src/screens/Categories/management/actions/CategoryActions.ts
@@ -1,20 +1,32 @@
 import ComponentActions from "@src/modules/StateManagement/Core/ComponentAction";
 import StateUtils from "@src/modules/StateManagement/Core/StateUtils";
 
-export default class CategoryActions extends StateUtils<Categories.State> {
+export default class CategoryActions extends StateUtils<Categories.State> implements Categories.Actions {
 	/**
 	 * THE GETTERS
-	 */
+	*/
 	getOptions() {
 		return this.state.filter.filters;
 	}
-
+	getCategoryGridData() {
+		throw new Error("Method not implemented.");
+	}
+	
+	/**
+	 * The setters 
+	 */
+	filterCategoryData(): Categories.CategoryGridData[] {
+		throw new Error("Method not implemented.");
+	}
+	
+	fetchCategoryGridData() {
+		throw new Error("Method not implemented.");
+	}
 	setQuery(query: string) {
 		this.mutateState((p) => {
 			p.filter.query = query;
 		});
 	}
-
 	toggleFilter(id: string) {
 		this.mutateState((p) => {
 			for (let filter of p.filter.filters) {
