fca8f8ee85963bfb7b1de457316c492e8da69904
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-05-10 19:10:54 +0530

    beautified the add global fields

--- src/forms/AddCompany/parts/ThirdPart/ThirdPart.tsx
+++ src/forms/AddCompany/parts/ThirdPart/ThirdPart.tsx
@@ -8,6 +8,7 @@ import ScaleOnHover from "@src/Components/interactions/ScaleOnHover/ScaleOnHover
 import FormCardHeader from "@src/Components/forms/FormCardHeader/FormCardHeader";
 import DefaultButton from "@src/Components/common/buttons/DefaultButton/DefaultButton";
 import NextButtonStyleConfig from "@src/Components/common/buttons/configurations/NextButtonStyle.config";
+import AddMore from "@src/Components/common/buttons/AddMore/AddMore";
 
 interface Props {}
 
@@ -21,102 +22,106 @@ export default function ThirdPart(props: Props) {
 					<FormCardHeader heading="Global Fields" subheading="Enter" />
 				</div>
 				<Card sx={{ padding: 3 }} variant="outlined">
-					<table className="w-full">
-						<thead
-							className="bg-slate-100 h-10 border-b border-slate-400"
-							style={{ boxSizing: "border-box" }}
-						>
-							<tr className="px-4 py-2">
-								<th>
-									<p className="text-md font-semibold text-slate-500">
-										Description
-									</p>
-								</th>
-								<th>
-									<p className="text-md font-semibold text-slate-500 ">Value</p>
-								</th>
-								<th className="w-fit"></th>
-							</tr>
-						</thead>
-
-						<tbody>
-							{state.descriptionLabels.map((v, i) => (
-								<tr className="mb-2 border-b" key={v.id}>
-									<td align="center">
-										<p className="text-md font-bold text-slate-700 py-5">
-											{v.key}
+					<div className="border rounded-md overflow-hidden">
+						<table className="w-full">
+							<thead
+								className="bg-slate-100 h-10 border-b border-slate-400"
+								style={{ boxSizing: "border-box" }}
+							>
+								<tr className="px-4 py-2">
+									<th>
+										<p className="text-md font-semibold text-slate-500">
+											Description
 										</p>
+									</th>
+									<th>
+										<p className="text-md font-semibold text-slate-500 ">
+											Value
+										</p>
+									</th>
+									<th className="w-fit"></th>
+								</tr>
+							</thead>
+
+							<tbody>
+								{state.descriptionLabels.map((v, i) => (
+									<tr className="mb-2 border-b" key={v.id}>
+										<td align="center">
+											<p className="text-md font-bold text-slate-700 py-5">
+												{v.key}
+											</p>
+										</td>
+										<td align="center" className="w-2/5">
+											<FieldInput
+												isValid={v.value.isValid}
+												error={v.value.error}
+												data={v.value.value}
+												onChange={(d) => {
+													thirdFormActions.updateField(d.target.value, i);
+												}}
+												type={"text"}
+												placeHolder={"enter value"}
+											/>
+										</td>
+										<td align="center" className="w-fit">
+											<div
+												onClick={() => {
+													thirdFormActions.deleteField(i);
+												}}
+											>
+												<RotateAndScale>
+													<AssetIndex.MinusCircleIcon />
+												</RotateAndScale>
+											</div>
+										</td>
+									</tr>
+								))}
+								<tr
+									className="mb-2 border-b"
+									style={{ borderTop: "5px solid transparent" }}
+								>
+									<td align="center" className="p-1">
+										<div className="p-2">
+											<FieldInput
+												width={"85%"}
+												isValid={state.descriptionEntry.key.isValid}
+												error={state.descriptionEntry.key.error}
+												data={state.descriptionEntry.key.value}
+												onChange={(d) => {
+													thirdFormActions.setAddKey(d.target.value);
+												}}
+												type={"text"}
+												placeHolder={"enter key"}
+											/>
+										</div>
 									</td>
-									<td align="center" className="w-2/5">
-										<FieldInput
-											isValid={v.value.isValid}
-											error={v.value.error}
-											data={v.value.value}
-											onChange={(d) => {
-												thirdFormActions.updateField(d.target.value, i);
-											}}
-											type={"text"}
-											placeHolder={"enter value"}
-										/>
-									</td>
-									<td align="center" className="w-fit">
-										<div
-											onClick={() => {
-												thirdFormActions.deleteField(i);
-											}}
-										>
-											<RotateAndScale>
-												<AssetIndex.MinusCircleIcon />
-											</RotateAndScale>
+									<td align="center" className="p-1" colSpan={2}>
+										<div className="p-2">
+											<FieldInput
+												width={"85%"}
+												isValid={state.descriptionEntry.value.isValid}
+												error={state.descriptionEntry.value.error}
+												data={state.descriptionEntry.value.value}
+												onChange={(d) => {
+													thirdFormActions.setAddValue(d.target.value);
+												}}
+												type={"text"}
+												placeHolder={"enter value"}
+											/>
 										</div>
 									</td>
 								</tr>
-							))}
-							<tr
-								className="mb-2 border-b"
-								style={{ borderTop: "5px solid transparent" }}
-							>
-								<td align="center" className="p-1">
-									<FieldInput
-										isValid={state.descriptionEntry.key.isValid}
-										error={state.descriptionEntry.key.error}
-										data={state.descriptionEntry.key.value}
-										onChange={(d) => {
-											thirdFormActions.setAddKey(d.target.value);
-										}}
-										type={"text"}
-										placeHolder={"enter key"}
-									/>
-								</td>
-								<td align="center" className="p-1">
-									<FieldInput
-										isValid={state.descriptionEntry.value.isValid}
-										error={state.descriptionEntry.value.error}
-										data={state.descriptionEntry.value.value}
-										onChange={(d) => {
-											thirdFormActions.setAddValue(d.target.value);
-										}}
-										type={"text"}
-										placeHolder={"enter value"}
-									/>
-								</td>
-								<td align="center" className="p-1">
-									<div
-										onClick={() => {
-											const verdict = thirdFormActions.validateAdd();
-											if (verdict) thirdFormActions.addField();
-										}}
-									>
-										<RotateAndScale config={{ scale: 1.05, rotate: 0 }}>
-											<button className="px-4 py-2 rounded-md bg-indigo-600 text-white">
-												Add
-											</button>
-										</RotateAndScale>
-									</div>
-								</td>
-							</tr>
-						</tbody>
-					</table>
+							</tbody>
+						</table>
+					</div>
+					<div className="crow jfe mt-5">
+						<AddMore
+							handleAdd={() => {
+								const verdict = thirdFormActions.validateAdd();
+								if (verdict) thirdFormActions.addField();
+							}}
+						/>
+					</div>
 				</Card>
 			</Card>
 			<div className="mt-8">
