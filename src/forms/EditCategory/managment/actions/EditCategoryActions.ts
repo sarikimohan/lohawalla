31bd7df3f4c9782c82fa70bebf71ee41f3b492f9
sarikimohan <sariki.mohankrishna@gmail.com>
2023-05-10 13:07:05 +0530

    validate editcategoryform

--- src/forms/EditCategory/managment/actions/EditCategoryActions.ts
+++ src/forms/EditCategory/managment/actions/EditCategoryActions.ts
@@ -17,6 +17,36 @@ export default class EditCategoryActions extends StateUtils<EditCategory.State> 
     })
   }
 
+  validateForm(){
+    let verdict = true;
+    const err: {
+			key?: string;
+			value?: string;
+		} = {
+			key: undefined,
+			value: undefined,
+		};
+
+    const req= "required"
+
+    if(this.state.categoryName.value ===""){
+      err.key = req;
+      verdict = false
+    }
+
+    if(this.state.categoryCode.value===""){
+      err.key= req;
+      verdict= false
+    }
+
+    if(this.state.description.value===""){
+      err.key = req;
+      verdict=false;
+    }
+
+    return verdict
+
+  }
   
   
 }
