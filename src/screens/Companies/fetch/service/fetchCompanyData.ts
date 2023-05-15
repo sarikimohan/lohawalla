a6368d865f041ce75dddd3a3c8aa5644f2815fd8
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-05-15 15:11:51 +0530

    updated the data

--- src/screens/Companies/fetch/service/fetchCompanyData.ts
+++ src/screens/Companies/fetch/service/fetchCompanyData.ts
@@ -1,19 +1,19 @@
 import { api } from "../Api";
 import CompaniesInstance from "../instance";
 
-
 export interface CompanyListRow {
-  _id: string;
-  srNo: number;
-  companyName: {
-    imageURL: string;
-    name: string;
-  };
-  price: number;
-  entryTime: string;
-  noOfProducts: number;
+	_id: string;
+	srNo: number;
+	companyName: {
+		imageURL: string;
+		name: string;
+	};
+	price: number;
+	entryTime: string;
+	noOfProducts: number;
+	rowStatus: RowStatus;
 }
 
-export default async function fetchCompanyData(){
-  return await CompaniesInstance.get<CompanyListRow[]>(api.getAllCompanies)
+export default async function fetchCompanyData() {
+	return await CompaniesInstance.get<CompanyListRow[]>(api.getAllCompanies);
 }
