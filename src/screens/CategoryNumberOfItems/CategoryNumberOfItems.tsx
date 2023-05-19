3449a04714956381101bef79babedfd14649b436
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-05-19 12:28:28 +0530

    completed category number of items

--- src/screens/CategoryNumberOfItems/CategoryNumberOfItems.tsx
+++ src/screens/CategoryNumberOfItems/CategoryNumberOfItems.tsx
@@ -9,6 +9,7 @@ import React, { useState, useEffect } from "react";
 import TableRow from "./components/TableRow/TableRow";
 import ServerActions from "./actions/ServerActions";
 import { useParams } from "react-router-dom";
+import LoadingBoundary from "@src/Components/common/LoadingBoundary/LoadingBoundary";
 
 interface Props {}
 
@@ -31,74 +32,74 @@ export default function CategoryNumberOfItems(props: Props) {
 		}
 	}, []);
 
-	console.log(state.grid);
-
 	return (
 		<div className="mx-6">
 			<div className="w-full">
 				<TitleNavBar title={"Category / Number of Item"} />
 			</div>
 			<div className={"p-7"}>
-				<Card variant="outlined" sx={{ padding: 5 }}>
-					<div>
-						<div className="crow mb-6">
-							<p className="subtitle fcolor-onyx">Number Of Items ()</p>
-						</div>
-						<div className="crow mb-6 sb">
-							<div className="d-flex vc">
-								<div className="mr-4">
-									<SearchBar onChange={(e) => {}} />
+				<LoadingBoundary asyncState={state.loading.fetch}>
+					<Card variant="outlined" sx={{ padding: 5 }}>
+						<div>
+							<div className="crow mb-6">
+								<p className="subtitle fcolor-onyx">Number Of Items ()</p>
+							</div>
+							<div className="crow mb-6 sb">
+								<div className="d-flex vc">
+									<div className="mr-4">
+										<SearchBar onChange={(e) => {}} />
+									</div>
+									<div>
+										<SearchFilters options={[]} onItemClick={() => {}} />
+									</div>
 								</div>
 								<div>
-									<SearchFilters options={[]} onItemClick={() => {}} />
+									<div className="flex">
+										<div className="mr-2">
+											<DefaultButton
+												onClick={function (): void {}}
+												label={"view margin"}
+											/>
+										</div>
+										<DefaultButton
+											onClick={function (): void {}}
+											label={"+ add items"}
+										/>
+									</div>
 								</div>
 							</div>
 							<div>
-								<div className="flex">
-									<div className="mr-2">
-										<DefaultButton
-											onClick={function (): void {}}
-											label={"view margin"}
-										/>
-									</div>
-									<DefaultButton
-										onClick={function (): void {}}
-										label={"+ add items"}
-									/>
-								</div>
+								<DefaultGrid
+									columns={[
+										{
+											name: "sr no",
+											width: 100,
+										},
+										"item name",
+										"price",
+										"no of companies",
+										"active company",
+										"inactive company",
+									]}
+								>
+									{state.grid.map((v, i) => (
+										<TableRow data={v} />
+									))}
+								</DefaultGrid>
 							</div>
 						</div>
-						<div>
-							<DefaultGrid
-								columns={[
-									{
-										name: "sr no",
-										width: 100,
-									},
-									"item name",
-									"price",
-									"no of companies",
-									"active company",
-									"inactive company",
-								]}
-							>
-								{state.grid.map((v, i) => (
-									<TableRow data={v} />
-								))}
-							</DefaultGrid>
+						<div className="mt-5 flex justify-end">
+							<DefaultButton
+								onClick={() => {}}
+								label={"save"}
+								defaultStyles={{
+									container: { background: "var(--iris)" },
+									text: { color: "#fff" },
+								}}
+							/>
 						</div>
-					</div>
-					<div className="mt-5 flex justify-end">
-						<DefaultButton
-							onClick={() => {}}
-							label={"save"}
-							defaultStyles={{
-								container: { background: "var(--iris)" },
-								text: { color: "#fff" },
-							}}
-						/>
-					</div>
-				</Card>
+					</Card>
+				</LoadingBoundary>
 			</div>
 		</div>
 	);
