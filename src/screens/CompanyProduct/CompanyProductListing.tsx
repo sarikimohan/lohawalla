22d83bb2bf255f3e91b6d8d8946aff3a54cf5f3f
Xydon <snehalkumar.singh.cse20@itbhu.ac.in>
2023-06-03 11:34:31 +0530

    company product touchup done

--- src/screens/CompanyProduct/CompanyProductListing.tsx
+++ src/screens/CompanyProduct/CompanyProductListing.tsx
@@ -12,13 +12,26 @@ import BannerContainer from "@src/Components/common/BannerContainer/BannerContai
 import RowContainer from "@src/Components/common/Grid/RowContainer.default";
 import CompanyProductsAction from "./managment/actions/CompanyProductAction";
 import { InitialState } from "./managment/state/initialState";
+import AsyncStateFactory from "@src/modules/StateManagement/AsyncState/AsyncStateFactory";
+import LoadingBoundary from "@src/Components/common/LoadingBoundary/LoadingBoundary";
+import LoadingWidget from "@src/Components/widget/LoadingWidget/LoadingWidget";
+import useHeight from "@src/modules/hooks/useHeight";
 
 function CompanyProductListing() {
 	const widthService = useWidth();
 	const { id } = useParams();
 	const [refresh, setRefresh] = useState(false);
 	const [showForm, setShowForm] = useState(false);
-	const [state, setState] = useState<CompanyProducts.State>(InitialState);
+	const [state, setState] = useState<CompanyProducts.State>({
+		products: [],
+		filter: {
+			query: "",
+			filters: [],
+		},
+		loading: {
+			fetch: AsyncStateFactory(),
+		},
+	});
 	const CompanyProductActions = new CompanyProductsAction(state, setState);
 
 	useEffect(() => {
@@ -27,49 +40,74 @@ function CompanyProductListing() {
 
 	if (!id) return <p className="text-xl">no id provided</p>;
 
-	console.log(state.products);
+	const heightHandle = useHeight();
+
+	const filteredList = CompanyProductActions.getFilteredList();
 
 	return (
 		<>
-			<div className={style.navContainer}>
+			<div className={style.navContainer} ref={heightHandle.ref}>
 				<BackNavBar title={"Category/Item/Company Product"} />
 			</div>
-			<div className={style.pageContainer}>
-				<Card className="p-8 w-100 mt-10" variant="outlined">
-					<div ref={widthService.ref}>
-						<div className="crow mb-6">
-							<p className="subtitle fcolor-onyx">Company Products ({})</p>
-						</div>
-						<div className="crow sb mb-6">
-							<div className="vc">
-								<div className="pr-4">
-									<SearchBar />
-								</div>
-								<div>
-									{/* <SearchFilters options={[{ id: '1', label: 'filter 1 ' }]} onChange={() => {}} /> */}
-								</div>
+			<LoadingBoundary
+				asyncState={state.loading.fetch}
+				loadingWidget={
+					<LoadingWidget height={`calc(100vh - ${heightHandle.height}px)`} />
+				}
+			>
+				<div
+					className={style.pageContainer + " p-14 bg-offWhite"}
+					style={{
+						height: `calc(100vh - ${heightHandle.height}px)`,
+						overflow: "auto",
+					}}
+				>
+					<Card
+						className="p-8 w-100 mt-10"
+						variant="outlined"
+						sx={{ borderRadius: "12px" }}
+					>
+						<div ref={widthService.ref}>
+							<div className="crow mb-6">
+								<p className="subtitle fcolor-onyx">
+									Company Products ({filteredList.length})
+								</p>
 							</div>
-							<DefaultButton
-								onClick={() => {
-									setShowForm(true);
-								}}
-								label={"+ add company product"}
+							<div className="crow sb mb-6">
+								<div className="vc">
+									<div className="pr-4">
+										<SearchBar
+											onChange={(e) => {
+												CompanyProductActions.setQuery(e);
+											}}
+										/>
+									</div>
+									<div>
+										{/* <SearchFilters options={[{ id: '1', label: 'filter 1 ' }]} onChange={() => {}} /> */}
+									</div>
+								</div>
+								<DefaultButton
+									onClick={() => {
+										setShowForm(true);
+									}}
+									label={"+ add company product"}
+								/>
+							</div>
+							<Grid<CompanyProducts.CompanyProduct>
+								data={filteredList}
+								config={columnConfig}
+								BannerContainer={(children) => (
+									<BannerContainer>{children}</BannerContainer>
+								)}
+								RowContainer={RowContainer<CompanyProducts.CompanyProduct>}
+								width={widthService.width}
+								paddingLeft={32}
+								paddingRight={32}
 							/>
 						</div>
-						<Grid<CompanyProducts.CompanyProduct>
-							data={state.products}
-							config={columnConfig}
-							BannerContainer={(children) => (
-								<BannerContainer>{children}</BannerContainer>
-							)}
-							RowContainer={RowContainer<CompanyProducts.CompanyProduct>}
-							width={widthService.width}
-							paddingLeft={32}
-							paddingRight={32}
-						/>
-					</div>
-				</Card>
-			</div>
+					</Card>
+				</div>
+			</LoadingBoundary>
 		</>
 	);
 }
